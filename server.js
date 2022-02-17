bcfdee419a9550236636819a09889cbfff364c19
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-02-17 14:46:05 -0500

    created functionality for all of the CRUD operations. Incorporating into Express.js routes next.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// imports the mysql2 package so we can connect to the MySQL DB
+const mysql = require('mysql2');
 // imports express
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -8,7 +10,57 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
+//Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'Zgray123456a!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
+
+
+// Query the db to test the connection
+//Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the err, which is the error response, and rows, which is the database query response.
+//If there are no errors in the SQL query, the err value is null.
+//This method is the key component that allows SQL commands to be written in a Node.js application.
+db.query(`SELECT * FROM candidates`, (err, rows) => { // Presents a full list of all candidates
+    // console.log(rows);
+  });
+
+  // GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    //   if (err) {
+    //       console.log(err);
+    //   }
+    //   console.log(row);
+  });
+
+  // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+
+// Create a candidate. Activates SQL and inputs the command.
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`; // the SQL command. We have 4 '?' placeholders
+const params = [1, 'Ronald', 'Firbank', 1]; // The SQL parameters
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
 // Default response for any other request (Not Found). Must be placed under all other routes.
 app.use((req, res) => {
